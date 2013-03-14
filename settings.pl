c05e2b290210572a45e397ed9f8ece37471cd3e5
Héctor Arroyo <churruka@hangar18.cc>
2013-03-15 00:27:28 +0100

    Be consistent between schema URLs

--- settings.pl
+++ settings.pl
@@ -31,7 +31,7 @@ $g_index_url = "$schema_base;f=admin/sql/CreateIndexes.sql;hb=master";
 $g_pk_url = "$schema_base;f=admin/sql/CreatePrimaryKeys.sql;hb=master";
 $g_func_url = "$schema_base;f=admin/sql/CreateFunctions.sql;hb=master";
 $g_pending_url = "$schema_base;f=admin/sql/ReplicationSetup.sql;hb=master";
-$g_stats_url = "$schema_base;f=admin/sql/statistics/CreateTables.sql";
+$g_stats_url = "$schema_base;f=admin/sql/statistics/CreateTables.sql;hb=master";
 
 # Replications URL
 $g_rep_host = "ftp.musicbrainz.org";
